c1a19e81aafc554e5a77de4c703ffda0d9a48c13
Scott Campbell <SLCampbell96@gmail.com>
2018-01-27 00:00:25 -0500

    Basic rotating cams

--- Assets/Scripts/CamBehavior.cs
+++ Assets/Scripts/CamBehavior.cs
@@ -4,13 +4,47 @@ using UnityEngine;
 
 public class CamBehavior : MonoBehaviour {
 
-	// Use this for initialization
+	public float RotationPause;
+	public float RotationBegin;
+	public float RotationEnd;
+
+	private bool isRotated;
+	private bool paused;
+	private float pauseTimer;
+	private Vector3 angles;
+
 	void Start () {
-		
+		isRotated = false;
+		paused = true;
+		angles = transform.eulerAngles;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(paused){
+			pauseTimer += Time.deltaTime;
+			if (pauseTimer >= 5){
+				paused = false;
+				pauseTimer = 0;
+			}
+		} else {
+			if(isRotated == false){
+				transform.Rotate(Vector3.up, Time.deltaTime * 20);
+				if(transform.eulerAngles.y > 90) {
+					angles.y = 90;
+					transform.eulerAngles = angles;
+					isRotated = true;
+					paused = true;					
+				}
+			} else {
+				transform.Rotate(Vector3.up, Time.deltaTime * -20);
+				if(transform.eulerAngles.y < .5 || transform.eulerAngles.y > 359.5 ) {
+					angles.y = 0;
+					transform.eulerAngles = angles;
+					isRotated = false;
+					paused = true;					
+				}			
+			}
+		}
 	}
 }
