604323028d97f26e55e3d0c4679de1804ab13a2c
Unknown <cal@yossman.net>
2018-01-27 12:06:00 -0500

    Camera Positioning

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Game : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        SceneManager.LoadScene("LevelTest", LoadSceneMode.Additive);
+      //  this.GetComponent<Camera>().enabled = true;
+      //  this.GetComponent<Camera>().gameObject.SetActive(true);
+
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+     //   Debug.Log("TEST");
+	}
+}
