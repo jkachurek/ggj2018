f924ace7ee977bb7cc7c619407f8d7e2f2663195
jkachurek <john.kachurek@gmail.com>
2018-01-26 23:08:25 -0500

    updated player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-
+	public float speed = 10;
+	
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +12,9 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+    var x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+    var z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
 		
+    transform.Translate(x, 0, z);
 	}
 }
