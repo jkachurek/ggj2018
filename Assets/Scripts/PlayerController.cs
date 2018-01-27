0efd468a8eb2f0703322528c1c0ea455f80dd177
jkachurek <john.kachurek@gmail.com>
2018-01-27 16:37:58 -0500

    Fixed controls

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,16 +5,17 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 	[SerializeField]
 	private float moveSpeed = 8;
-
+	[SerializeField]
+	private float turnSpeed = 10;
 	// Update is called once per frame
 	void Update () {
 		var x = Input.GetAxis("Horizontal");
 		var z = Input.GetAxis("Vertical");
-		var rotateVector = new Vector3(x, 0, z);
+		var rotateVector = new Vector3(0, x, 0);
+		transform.Translate(new Vector3(0, 0, z) * Time.deltaTime * moveSpeed);
 		if (rotateVector != Vector3.zero)
 		{
-			transform.rotation = Quaternion.LookRotation(rotateVector);
-			transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
+			transform.Rotate(rotateVector * Time.deltaTime * turnSpeed);			
 		}
 	}
 }
