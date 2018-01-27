14248d34211b120834db940272123b9bbc0b7513
Scott Campbell <SLCampbell96@gmail.com>
2018-01-27 15:22:44 -0500

    Robot With Walking and Idle

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,15 +6,22 @@ public class PlayerController : MonoBehaviour {
 	[SerializeField]
 	private float moveSpeed = 8;
 
+	Animator anim;
+	void Start(){
+		anim = GetComponent<Animator>();
+	}
+
 	// Update is called once per frame
 	void Update () {
 		var x = Input.GetAxis("Horizontal");
 		var z = Input.GetAxis("Vertical");
 		var rotateVector = new Vector3(x, 0, z);
+		anim.SetBool("Walking", false);		
 		if (rotateVector != Vector3.zero)
 		{
 			transform.rotation = Quaternion.LookRotation(rotateVector);
 			transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
+			anim.SetBool("Walking", true);
 		}
 	}
 }
