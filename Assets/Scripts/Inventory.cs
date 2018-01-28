e10c52e4ae7bd2a5b823b1b3a513999ca9454607
jkachurek <john.kachurek@gmail.com>
2018-01-28 01:21:03 -0500

    Keys unlock doors, made door prefab

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -3,29 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Inventory : MonoBehaviour {
-	private List<GameObject> _inventory;
+	private Dictionary<string, int> _inventory;
 
 
 	// Use this for initialization
 	void Start () {
-		_inventory = new List<GameObject>();
+		_inventory = new Dictionary<string, int>();
 	}
 
 	public void AddItem (GameObject item)
 	{
-		_inventory.Add(item);
-		Debug.Log("Item collected!");
+		if (_inventory.ContainsKey(item.name))
+			_inventory[item.name]++;
+		else
+			_inventory.Add(item.name, 1);
+		Debug.Log(item.name + " collected!");
 	}
-	public void RemoveItem (GameObject item)
+	public void RemoveItem (string itemName)
 	{
-		_inventory.Remove(item);
+		if (_inventory.ContainsKey(itemName) && _inventory[itemName] > 0)
+			_inventory[itemName]--;
+		else
+			Debug.Log("Not enough of " + itemName);
 	}
-	public bool HasItem (System.Type type)
+	public bool HasItem (string itemName)
 	{
-		return _inventory.Exists(o => o.GetType() == type);
-	}
-	public GameObject GetItem(System.Type type)
-	{
-		return _inventory.Find(o => o.GetType() == type);
+		return _inventory.ContainsKey(itemName) && _inventory[itemName] > 0;
 	}
 }
