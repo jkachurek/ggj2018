461bb38598d8fc16c04f27340b5f8b77702605f4
Scott Campbell <SLCampbell96@gmail.com>
2018-01-28 11:16:13 -0500

    Added menu scene

---
+++ Assets/Scripts/HideImage.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HideImage : MonoBehaviour {
+	public void HideSelf(){
+		this.GetComponent<Image>().enabled = false;
+	}
+	public void ShowSelf(){
+		this.GetComponent<Image>().enabled = true;	
+	}
+}
