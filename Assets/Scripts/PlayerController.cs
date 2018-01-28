34f681648c8873b37cbcb5195cd30cb8a99c6c7e
jkachurek <john.kachurek@gmail.com>
2018-01-27 23:09:01 -0500

    Fixed playerController to use rigidbody physics
    Collisions now work properly!!

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,29 +4,53 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 	[SerializeField]
-	private float moveSpeed = 8;
+	private float moveSpeed = 20f;
 	[SerializeField]
-	private float turnSpeed = 10;
+	private float turnSpeed = 10f;
 
-	Animator anim;
+
+	Quaternion targetRotation;
+
+
+	private float inputX = 0f;
+	private float inputY = 0f;
+	
+
+	Animator _anim;
+	Rigidbody _rigid;
+
 	void Start(){
-		anim = GetComponent<Animator>();
+		_anim = GetComponent<Animator>();
+		_rigid = GetComponent<Rigidbody>();
 	}
 
 	// Update is called once per frame
 	void Update () {
-		var x = Input.GetAxis("Horizontal");
-		var z = Input.GetAxis("Vertical");
-		var rotateVector = new Vector3(0, x, 0);
-		transform.Translate(new Vector3(0, 0, z) * Time.deltaTime * moveSpeed);
-		if (x == 0 && z == 0)
-			anim.SetBool("Walking", false);
-		else
-			anim.SetBool("Walking", true);
-		if (rotateVector != Vector3.zero)
-		{
-			transform.Rotate(rotateVector * Time.deltaTime * turnSpeed);			
-			
-		}
+		inputX = Input.GetAxis("Horizontal");
+		inputY = Input.GetAxis("Vertical");
+
+		
+
+		float rotation = inputX * turnSpeed * Time.deltaTime;
+		transform.Rotate(0, rotation, 0);
+		//_rigid.MoveRotation(Quaternion.LookRotation(new Vector3(0, rotation, 0)));
+		//_inputs.x = Input.GetAxis("Horizontal");
+		//_inputs.z = Input.GetAxis("Vertical");
+
+		//if (_inputs != Vector3.zero)
+		//_anim.SetBool("Walking", false);
+		//else
+		//_anim.SetBool("Walking", true);
+	}
+	private void FixedUpdate()
+	{
+		var movement = new Vector3(0, 0, inputY);
+		_rigid.MovePosition(_rigid.position + transform.forward * inputY * moveSpeed * Time.deltaTime);
+		//float movement = inputY * moveSpeed * Time.fixedDeltaTime;
+		//var movement = new Vector3(inputX, 0);
+		//_rigid.MovePosition(_rigid.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+		//float rotation = inputX * turnSpeed * Time.fixedDeltaTime;
+		//transform.Rotate(0, rotation, 0);
 	}
 }
